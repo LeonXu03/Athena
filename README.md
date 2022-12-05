4813a50edfe7a8ca9fc47c92c32038fcdadc09fe
LeonXu03 <leonxu0407@outlook.com>
2022-12-04 23:33:25 -0500

    database seeds setup

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # Athena Student Planner
+
 Athena is a planning tool that combines the traditional calendar with a ticketing system. By students for students. For more information, please refer to the [docs](https://www.notion.so/Student-Planner-d846991fccc54418981b09f31fbb4fd9).
+
 ## Maintainers
+
 Leon Xu, Johnny Cao, Harris Chong
+
 ## Git Development Flow
+
 Trunk based development. DO NOT directly push to main branch. Create a branch and then create a PR for adding new features.
+
+## Install Prerequisites
+
+1. Install MySQL + MySQL Workbench (Windows tutorial: https://www.youtube.com/watch?v=2c2fUOgZMmY&ab_channel=AmitThinks)
+
+   a. Please set your mySQL root password to `xxxx` during setup
+
+2. Install node and npm via nvm version manager (https://github.com/coreybutler/nvm-windows)
+3. Install Go (https://go.dev/doc/install)
+
+## Start Athena
+
+1. To setup MySQL database: `go run main.go`
---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	username = "root"
+	password = "xxxx" //this is the password set on MySQL installation
+	hostname = "127.0.0.1:3306"
+	dbname   = "athena"
+)
+
+// Create data source name for DB connection
+func createDSN(dbName string) string {
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, hostname, dbName)
+}
+
+// Create database with dbname
+func createDB(conn *sql.DB) error {
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+
+	log.Printf("Dropping %s db if it exists", dbname)
+	_, err := conn.ExecContext(ctx, "DROP DATABASE IF EXISTS "+dbname)
+	if err != nil {
+		log.Printf("Error %s when dropping DB %s\n", err, dbname)
+		return err
+	}
+
+	log.Printf("Creating %s db", dbname)
+	_, err = conn.ExecContext(ctx, "CREATE DATABASE IF NOT EXISTS "+dbname)
+	if err != nil {
+		log.Printf("Error %s when creating DB %s\n", err, dbname)
+		return err
+	}
+	log.Printf("Successfully created %s database", dbname)
+
+	return nil
+}
+
+// create seed tables for athena db
+func createBasicTables(athenaDB *sql.DB) error {
+	log.Print("Creating user table")
+
+	query := `CREATE TABLE IF NOT EXISTS user(
+		id BINARY(16) PRIMARY KEY,
+		firstName VARCHAR(100) NOT NULL,
+		lastName VARCHAR(100) NOT NULL,
+		fullName VARCHAR(100) NOT NULL,
+		username VARCHAR(100) NOT NULL,
+		password VARCHAR(100) NOT NULL,
+		createdAt datetime default CURRENT_TIMESTAMP, 
+		updatedAt datetime default CURRENT_TIMESTAMP)`
+
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	_, err := athenaDB.ExecContext(ctx, query)
+	if err != nil {
+		log.Printf("Error %s when creating user table", err)
+		return err
+	}
+	log.Print("Created user table successfully")
+
+	return nil
+}
+
+// get athena db connection
+func getAthenaDB() (*sql.DB, error) {
+	//connect to athena db
+	athenaDB, err := sql.Open("mysql", createDSN(dbname))
+	if err != nil {
+		log.Printf("Error %s when opening athena DB", err)
+		return nil, err
+	}
+
+	//ping to check connection to athena db
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	err = athenaDB.PingContext(ctx)
+	if err != nil {
+		log.Printf("Errors %s pinging DB", err)
+		return nil, err
+	}
+	log.Printf("Connected to DB %s successfully\n", dbname)
+
+	return athenaDB, nil
+}
+
+func main() {
+	fmt.Println("Start setting up Athena MySQL database")
+
+	//open & return connection to MySQL
+	conn, err := sql.Open("mysql", createDSN(""))
+	if err != nil {
+		log.Printf("Error %s when connecting to MySQL\n", err)
+		return
+	}
+	defer conn.Close()
+
+	//create athena db
+	err = createDB(conn)
+	if err != nil {
+		log.Printf("Error creating db")
+		return
+	}
+	conn.Close()
+
+	//connect to athena db
+	athenaDB, err := getAthenaDB()
+	if err != nil {
+		log.Printf("Error connecting to athena db")
+		return
+	}
+
+	//generate basic tables
+	err = createBasicTables(athenaDB)
+	if err != nil {
+		log.Printf("Failed creating basic tables with error %s", err)
+		return
+	}
+}
